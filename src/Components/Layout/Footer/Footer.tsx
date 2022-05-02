c03da389eed9b0cd8a9ceb4994dbfe39acf22e41
yathushanSliit <rajendranyathushan3@gmail.com>
2022-05-02 19:08:16 +0530

    my test

--- src/Components/Layout/Footer/Footer.tsx
+++ src/Components/Layout/Footer/Footer.tsx
@@ -4,7 +4,7 @@ let Footer = ()=>{
     return(
         <div>
         <footer className="main-footer">
-          <strong>Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+          <strong>Copyright © 2014-2019 <a href="http://Vitalmasks.com">Vital Masks</a>.</strong>
           All rights reserved.
           <div className="float-right d-none d-sm-inline-block">
             <b>Version</b> 3.0.0
