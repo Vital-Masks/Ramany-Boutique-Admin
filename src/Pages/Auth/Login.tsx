c5f49a4c431e6913677667f2cf6d2a9c389990f4
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2022-07-30 17:47:47 +0530

    fix add image and route

--- src/Pages/Auth/Login.tsx
+++ src/Pages/Auth/Login.tsx
@@ -1,108 +1,114 @@
-import React, { useRef, useState, useEffect, useContext } from 'react';
-import { Link, Navigate } from "react-router-dom";
-import PropTypes from 'prop-types';
-import AuthService from '../../Services/AuthService'
-import AuthContext from '../../Context/AuthProvider'
+import React, { useRef, useState, useEffect, useContext } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import AuthService from "../../Services/AuthService";
+import AuthContext from "../../Context/AuthProvider";
 
+let Login = ({ setToken }) => {
+	const navigate = useNavigate();
+	// const { setAuth } : any = useContext(AuthContext);
+	// const userRef = useRef();
+	// const errRef = useRef();
 
-let Login = ({setToken}) => {
+	const [username, setUserName] = useState("");
+	const [password, setPassword] = useState("");
 
-    // const { setAuth } : any = useContext(AuthContext);
-    // const userRef = useRef();
-    // const errRef = useRef();
+	const [errMsg, setErrMsg] = useState("");
+	const [success, setSuccess] = useState(false);
 
-    const [username, setUserName] = useState("");
-    const [password, setPassword] = useState("");
+	// useEffect(() => {
+	//     userRef.current.focus();
+	// }, [])
 
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+	// useEffect(() => {
+	//     setErrMsg('');
+	// }, [username, password])
 
-    // useEffect(() => {
-    //     userRef.current.focus();
-    // }, [])
+	// const [loggedInUser, setloggedInUser] = useState({});
 
-    // useEffect(() => {
-    //     setErrMsg('');
-    // }, [username, password])
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		AuthService.AutheticateUser({
+			username,
+			password,
+		}).then((response) => {
+			if (response.data.token) {
+				// setloggedInUser(response)
+				setToken(response.data.token);
+				// setAuth({ username });
+				navigate("/");
+			} else {
+			}
+		});
+	};
 
-    // const [loggedInUser, setloggedInUser] = useState({});
+	return (
+		<body className="hold-transition login-page">
+			<div className="login-box">
+				<div className="card card-outline card-primary">
+					<div className="card-header text-center">
+						<a className="h1">
+							<b>Ramany Boutique</b> ADMIN
+						</a>
+					</div>
+					<div className="card-body">
+						<p className="login-box-msg">Sign in to start your session</p>
+						<form method="post">
+							<div className="input-group mb-3">
+								<input
+									type="test"
+									className="form-control"
+									placeholder="Email"
+									onChange={(e) => setUserName(e.target.value)}
+								></input>
+								<div className="input-group-append">
+									<div className="input-group-text">
+										<span className="fas fa-envelope"></span>
+									</div>
+								</div>
+							</div>
+							<div className="input-group mb-3">
+								<input
+									type="password"
+									className="form-control"
+									placeholder="Password"
+									onChange={(e) => setPassword(e.target.value)}
+								></input>
+								<div className="input-group-append">
+									<div className="input-group-text">
+										<span className="fas fa-lock"></span>
+									</div>
+								</div>
+							</div>
+							<div className="row">
+								<div className="col-8">
+									<div className="icheck-primary">
+										<input type="checkbox" id="remember"></input>
+										<label htmlFor="remember">Remember Me</label>
+									</div>
+								</div>
+							</div>
+						</form>
+						<div className="social-auth-links text-center mt-2 mb-3">
+							<a href="#" className="btn btn-block btn-primary">
+								<button
+									type="submit"
+									onClick={handleSubmit}
+									className="btn btn-primary btn-block"
+								>
+									Sign In
+								</button>
+							</a>
+						</div>
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        AuthService.AutheticateUser({
-            username,
-            password
-        }).then(response => {
-            if(response.data.token){
-                // setloggedInUser(response)
-                setToken(response.data.token)
-                // setAuth({ username });
-              return <Navigate to="/dashboard" replace={true} />
-                
-            }
-            else{
-
-            }
-            
-        })
-    }
-
-    return (
-        <body className="hold-transition login-page">
-            <div className="login-box">
-
-                <div className="card card-outline card-primary">
-                    <div className="card-header text-center">
-                        <a className="h1"><b>Ramany Boutique</b> ADMIN</a>
-                    </div>
-                    <div className="card-body">
-                        <p className="login-box-msg">Sign in to start your session</p>
-                        <form method="post">
-                            <div className="input-group mb-3">
-                                <input type="test" className="form-control" placeholder="Email" onChange={e => setUserName(e.target.value)} ></input>
-                                <div className="input-group-append">
-                                    <div className="input-group-text">
-                                        <span className="fas fa-envelope"></span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="input-group mb-3">
-                                <input type="password" className="form-control" placeholder="Password" onChange={e => setPassword(e.target.value)}></input>
-                                <div className="input-group-append">
-                                    <div className="input-group-text">
-                                        <span className="fas fa-lock"></span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-8">
-                                    <div className="icheck-primary">
-                                        <input type="checkbox" id="remember"></input>
-                                        <label htmlFor="remember">
-                                            Remember Me
-                                        </label>
-                                    </div>
-                                </div>
-
-
-                            </div>
-                        </form>
-                        <div className="social-auth-links text-center mt-2 mb-3">
-                            <a href="#" className="btn btn-block btn-primary">
-                                <button type="submit" onClick={handleSubmit} className="btn btn-primary btn-block">Sign In</button>
-                            </a>
-                        </div>
-
-                        <p className="mb-1">
-                            <a href="forgot-password.html">I forgot my password</a>
-                        </p>
-                    </div>
-
-                </div>
-
-            </div>
-        </body>
-    );
-}
+						<p className="mb-1">
+							<a href="forgot-password.html">I forgot my password</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</body>
+	);
+};
 
 export default Login;
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes as Switch, Route, Navigate, useNavigate } from "react-router-dom";
 import Layout from "../Components/Layout/Layout";
 import {
 	Dashboard,
@@ -30,34 +30,37 @@ function getToken() {
 
 function Routes() {
 	const token = getToken();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!token) {
+			navigate("/login");
+		}
+	}, []);
 
 	const layouts = () => {
 		return (
-			<Router>
-				<Layout>
-					<Switch>
-						{/* <Route path="/login" element={<Login />} /> */}
-						<Route path="/dashboard" element={<Dashboard />} />
-						<Route path="/viewCloths" element={<ViewCloths />} />
-						<Route path="/addCloth" element={<AddCloth />} />
-						<Route path="/updateCloth" element={<UpdateCloth />} />
-						<Route path="/viewJewellerys" element={<ViewJewellerys />} />
-						<Route path="/addJewellery" element={<AddJewellery />} />
-						<Route path="/updateJewellery" element={<UpdateJewellery />} />
-						<Route path="/categories" element={<Categories />} />
-					</Switch>
-				</Layout>
-			</Router>
+			<Layout>
+				<Switch>
+					<Route path="/" element={<Dashboard />} />
+					<Route path="/viewCloths" element={<ViewCloths />} />
+					<Route path="/addCloth" element={<AddCloth />} />
+					<Route path="/updateCloth" element={<UpdateCloth />} />
+					<Route path="/viewJewellerys" element={<ViewJewellerys />} />
+					<Route path="/addJewellery" element={<AddJewellery />} />
+					<Route path="/updateJewellery" element={<UpdateJewellery />} />
+					<Route path="/categories" element={<Categories />} />
+					<Route path="/login" element={<Login setToken={setToken} />} />
+				</Switch>
+			</Layout>
 		);
 	};
 
 	const authLayout = () => {
 		return (
-			<Router>
-				<Switch>
-					<Route path="/login" element={<Login setToken={setToken} />} />
-				</Switch>
-			</Router>
+			<Switch>
+				<Route path="/login" element={<Login setToken={setToken} />} />
+			</Switch>
 		);
 	};
 
