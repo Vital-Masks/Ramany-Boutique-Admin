8c480d212b9a507d546bcef6fcfe08f3fa2ec1b5
yathushansliit <30892776+yathushansliit@users.noreply.github.com>
2022-09-29 22:13:05 +0530

    done

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: `http://localhost:3000/api`
+  baseURL: `http://localhost:3000/api`
 
-  baseURL: `https://dev-ramany-boutique-api.herokuapp.com/api`,
+  // baseURL: `https://dev-ramany-boutique-api.herokuapp.com/api`,
 });
