e2f5c325d0449181862814dfe3b7bf88b43650d8
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2022-09-30 22:11:52 +0530

    change api url

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: `http://localhost:3000/api`
-
-  baseURL: `https://dev-ramany-boutique-api.herokuapp.com/api`,
+  baseURL: `https://api.theramyaboutique.com/api/`,
 });
