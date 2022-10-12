547a7d10bd026de003cfe2930ab01f06b22ba098
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2022-10-12 19:41:13 +0530

    base64 image method

--- src/Pages/Cloth/AddCloth.tsx
+++ src/Pages/Cloth/AddCloth.tsx
@@ -89,17 +89,17 @@ const AddCloth = () => {
 
       // Convert the file to base64 text
       reader.readAsDataURL(file);
-      baseURL = URL.createObjectURL(file);
-      resolve(baseURL);
+      // baseURL = URL.createObjectURL(file);
+      // resolve(baseURL);
       // on reader load somthing...
-      // reader.onload = () => {
-      //   // Make a fileInfo Object
-      //   // console.log("Called", reader);
-      //   baseURL = reader.result;
+      reader.onload = () => {
+        // Make a fileInfo Object
+        // console.log("Called", reader);
+        baseURL = reader.result;
 
-      //   // console.log(baseURL);
-      //   resolve(baseURL);
-      // };
+        // console.log(baseURL);
+        resolve(baseURL);
+      };
       //   console.log(fileInfo);
     });
   };
