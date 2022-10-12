6bff74bf5ce5b7e6964d2340405b84446622fb40
Ginthozan Varnakulasingam <ginthozanv@gmail.com>
2022-10-12 23:39:26 +0530

    blob image

--- src/Pages/Cloth/AddCloth.tsx
+++ src/Pages/Cloth/AddCloth.tsx
@@ -84,23 +84,8 @@ const AddCloth = () => {
   const getBase64 = (file) => {
     return new Promise((resolve) => {
       let baseURL: any;
-      // Make new FileReader
-      let reader = new FileReader();
-
-      // Convert the file to base64 text
-      reader.readAsDataURL(file);
-      // baseURL = URL.createObjectURL(file);
-      // resolve(baseURL);
-      // on reader load somthing...
-      reader.onload = () => {
-        // Make a fileInfo Object
-        // console.log("Called", reader);
-        baseURL = reader.result;
-
-        // console.log(baseURL);
-        resolve(baseURL);
-      };
-      //   console.log(fileInfo);
+      baseURL = URL.createObjectURL(file);
+      resolve(baseURL);
     });
   };
 
